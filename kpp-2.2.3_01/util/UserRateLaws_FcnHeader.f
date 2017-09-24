6588c6519b9bd7dc5618c56587938ac40af991e9
Josue Bock <josue.bock@meteo.fr>
2017-09-24 20:57:12 +0200

    Mistra specific: rate law function definitions and headers are in Mistra files

--- kpp-2.2.3_01/util/UserRateLaws_FcnHeader.f
+++ kpp-2.2.3_01/util/UserRateLaws_FcnHeader.f
@@ -1,6 +1,7 @@
 C  Definitions for User-defined Rate Law functions
 C  To be inserted in funcions that call rate laws
 
-      KPP_REAL ARR, ARR2
-      KPP_REAL EP2, EP3, FALL
-      EXTERNAL ARR, ARR2, EP2, EP3, FALL
+C jjb 05-07-2016
+C
+C removed along with the rate law functions
+C original file copied in util/UserRateLaw_FcnHeader.f
