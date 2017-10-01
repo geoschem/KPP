geoschem/KPP

commit 32b4664a4aecd6393591e5b276007cbd4370fa81

Minor improvements to rectify compilation warnings.   - explicit declarations (in *.h files when possible)   - unused variables declarations

kpp-2.2.3_01/src/gen.c
@@ -365,12 +365,13 @@ int  *lookat;
 int  *moni;
 char *snames[MAX_SPECIES];
 int  *trans;
-char *strans[MAX_SPECIES];
+/*char *strans[MAX_SPECIES];*/ /*jjb 12072017 defined but unused*/
 char *smass[MAX_ATOMS];
 char *seqn[MAX_EQN];
 char *bufeqn, *p;
 int dim;
 int flxind[MAX_EQN];
+int EqnString( int eq, char * buf );
 
 
   /* Allocate local data structures */
@@ -441,7 +442,7 @@ int flxind[MAX_EQN];
   for (i = 0; i < SpcNr; i++)
     if ( SpeciesTable[Code[i]].trans ) {
       trans[ntrans] = Index(i);
-      strans[ntrans] = SpeciesTable[Code[i]].name;
+      /*strans[ntrans] = SpeciesTable[Code[i]].name;*/ /*jjb 12072017 defined but unused*/
       ntrans++;
     }
 
@@ -671,10 +672,10 @@ int F_VAR, FSPLIT_VAR;
     }
     for (i = VarNr; i < VarNr; i++) {
       sum = Const(0);
-      for (j = 0; j < EqnNr; j++) 
+      for (j = 0; j < EqnNr; j++)
         sum = Add( sum, Mul( Const( Stoich[i][j] ), Elm( A, j ) ) );
       Assign( Elm( Vdot, i ), sum );
-    }    
+    }
 
   } else {
 
@@ -694,7 +695,7 @@ int F_VAR, FSPLIT_VAR;
     /* msl_20160421
     for (i = 0; i < VarNr; i++) {
       sum = Const(0);
-      for (j = 0; j < EqnNr; j++) 
+      for (j = 0; j < EqnNr; j++)
         sum = Add( sum, Mul( Const( Stoich_Left[i][j] ), Elm( A, j ) ) );
       Assign( Elm( D_VAR, i ), sum );
     }
@@ -740,14 +741,12 @@ int F_VAR, FSPLIT_VAR;
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void GenerateFlux()
 {
-int i, j, k;
-int used;
-int l, m;
+int i, j;
 int FLUX_VAR;
 
   if( VarNr == 0 ) return;
-  
-  /*  if (useLang != MATLAB_LANG)  /* Matlab generates an additional file per function */
+
+  /*  if (useLang != MATLAB_LANG) */ /* Matlab generates an additional file per function */
   /*     UseFile( functionFile ); */
 
   FLUX_VAR = DefFnc( "Flux", 3, "calculate production & loss terms from reaction flux");
@@ -759,21 +758,21 @@ int FLUX_VAR;
 
   NewLines(1);
   WriteComment("Production function");
-  
+
   for (i = 0; i < VarNr; i++) {
     sum = Const(0);
-    for (j = 0; j < EqnNr; j++) 
+    for (j = 0; j < EqnNr; j++)
       sum = Add( sum, Mul( Const( Stoich_Right[i][j] ), Elm( RR, j ) ) );
     Assign( Elm( P_VAR, i ), sum );
   }
-  
+
   NewLines(1);
   WriteComment("Destruction function");
-  
+
   /* msl_20160421 */
   for (i = 0; i < VarNr; i++) {
     sum = Const(0);
-    for (j = 0; j < EqnNr; j++) 
+    for (j = 0; j < EqnNr; j++)
       sum = Add( sum, Mul( Const( Stoich_Left[i][j] ), Elm( RR, j ) ) );
     Assign( Elm( D_VAR, i ), sum );
   }
@@ -1849,7 +1848,7 @@ int *irow;
 int *icol;
 int *crow;
 int *diag;
-int nElm;
+/*int nElm;*/ /*jjb 12072017 defined but unused*/
 int ibgn, iend;
 int useLangOld;
 int dim;
@@ -1865,7 +1864,7 @@ int dim;
 
   useLangOld = useLang;
   useLang = C_LANG;
-  nElm = NonZero( LU, 0, VarNr, irow, icol, crow, diag );
+  /*nElm = NonZero( LU, 0, VarNr, irow, icol, crow, diag );*/ /*jjb 12072017 defined but unused*/
   useLang = useLangOld;
 
   UseFile( linalgFile );
@@ -1918,7 +1917,7 @@ int *irow;
 int *icol;
 int *crow;
 int *diag;
-int nElm;
+/*int nElm;*/ /*jjb 12072017 defined but unused*/
 int ibgn, iend;
 int useLangOld;
 int **pos;
@@ -1936,7 +1935,7 @@ int dim;
 
   useLangOld = useLang;
   useLang = C_LANG;
-  nElm = NonZero( LU, 0, VarNr, irow, icol, crow, diag );
+  /*nElm = NonZero( LU, 0, VarNr, irow, icol, crow, diag );*/ /*jjb 12072017 defined but unused*/
   useLang = useLangOld;
 
   UseFile( linalgFile );
@@ -2712,7 +2711,7 @@ int INITVAL;
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void GenerateShuffle_user2kpp()
 {
-int i,k,l;
+int i,k;
 int Shuffle_user2kpp;
 
   UseFile( utilFile );
@@ -2720,7 +2719,7 @@ int Shuffle_user2kpp;
   Shuffle_user2kpp    = DefFnc( "Shuffle_user2kpp", 2, "function to copy concentrations from USER to KPP");
   FunctionBegin( Shuffle_user2kpp, V_USER, V );
 
-  k = 0;l = 0;
+  k = 0;
   for( i = 1; i < SpcNr; i++) {
     if( ReverseCode[i] < 0 ) {
       if( SpeciesTable[i].type == VAR_SPC ) k++;
@@ -2747,7 +2746,7 @@ int Shuffle_user2kpp;
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 void GenerateShuffle_kpp2user()
 {
-int i,k,l;
+int i,k;
 int Shuffle_kpp2user;
 
   UseFile( utilFile );
@@ -2755,7 +2754,7 @@ int Shuffle_kpp2user;
   Shuffle_kpp2user    = DefFnc( "Shuffle_kpp2user", 2, "function to restore concentrations from KPP to USER");
   FunctionBegin( Shuffle_kpp2user, V, V_USER );
 
-  k = 0; l = 0;
+  k = 0;
   for( i = 0; i < SpcNr; i++) {
     if( ReverseCode[i] < 0 ) {
       if( SpeciesTable[i].type == VAR_SPC ) k++;

kpp-2.2.3_01/src/code.c
@@ -32,6 +32,7 @@
 
 #include "gdata.h"
 #include "code.h"
+#include "scan.h"
 #include <unistd.h>
 #include <string.h>
 
@@ -108,10 +109,10 @@ FILE *oldf;
       printf("\n\nKPP Warning (internal): trying to UseFile NULL file pointer!\n");
     }
     oldf = currentFile;
-    currentFile = file; 
+    currentFile = file;
     return oldf;
 }
-  
+
 void OpenFile( FILE **fpp, char *name, char * ext, char * identity )
 {
 char bufname[200];
@@ -120,14 +121,14 @@ time_t t;
 int blength;
 
   time( &t );
-  sprintf( bufname, "%s%s", name, ext );  
+  sprintf( bufname, "%s%s", name, ext );
   if( *fpp ) fclose( *fpp );
   *fpp = fopen( bufname, "w" );
-  if ( *fpp == 0 ) 
+  if ( *fpp == 0 )
     FatalError(3,"%s: Can't create file", bufname );
- 
+
   UseFile( *fpp );
-  
+
   WriteDelim();
   WriteComment("");
   WriteComment("%s",identity);
@@ -144,7 +145,7 @@ int blength;
   WriteComment("       R. Sander, Max-Planck Institute for Chemistry, Mainz, Germany");
   WriteComment("");
   WriteComment("%-20s : %s", "File", bufname );
-  strcpy( buf, (char*)ctime( &t ) ); 
+  strcpy( buf, (char*)ctime( &t ) );
   buf[ (int)strlen(buf) - 1 ] = 0;
   WriteComment("%-20s : %s", "Time", buf );
   WriteComment("%-20s : %s", "Working directory", getcwd(buf, 200) );
@@ -152,7 +153,7 @@ int blength;
   WriteComment("%-20s : %s", "Output root filename", rootFileName );
   WriteComment("");
   WriteDelim();
-  NewLines(1);  
+  NewLines(1);
 /* Include Headers in .c  Files, except Makefile */
   blength = strlen(bufname);
   if ( (bufname[blength-2]=='.')&&(bufname[blength-1]=='c') ) {
@@ -164,7 +165,7 @@ int blength;
     C_Inline("#include \"%s_Global.h\"", rootFileName);
     if( useJacSparse )
        C_Inline("#include \"%s_Sparse.h\"", rootFileName);
-    }   
+    }
   NewLines(2);
 }
 
@@ -181,7 +182,7 @@ Va_list args;
   Va_start( args, fmt );
   vsprintf( outBuffer, fmt, args );
   va_end( args );
-  outBuffer += strlen( outBuffer ); 
+  outBuffer += strlen( outBuffer );
 }
 
 void FlushBuf()
@@ -210,19 +211,19 @@ void WriteDelim()
 {
 /*
   WriteComment("****************************************************************");
-*/ 
+*/
   WriteComment("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 }
 
 void NewLines( int n )
 {
-  for( ; n > 0; n-- ) 
+  for( ; n > 0; n-- )
     bprintf("\n");
 
-  FlushBuf();  
+  FlushBuf();
 }
 
-void IncludeFile( char * fname ) 
+void IncludeFile( char * fname )
 {
 FILE *fp;
 #define MAX_LINE 200
@@ -234,13 +235,13 @@ char line[ MAX_LINE ];
     FatalError(3,"%s: Can't read file", fname );
 
   FlushBuf();
-  
+
   while( !feof(fp) ) {
     *line = '\0';
     fgets( line, MAX_LINE, fp );
     fputs( line, currentFile );
   }
-  
+
   fclose( fp );
 }
 
@@ -256,7 +257,7 @@ FILE * fp;
   vsprintf( buf, fmt, args );
   va_end( args );
 
-  switch( useLang ) { 
+  switch( useLang ) {
     case F77_LANG: sprintf( buf, "%s.f", buf );
                  break;
     case F90_LANG: sprintf( buf, "%s.f90", buf );
@@ -265,42 +266,42 @@ FILE * fp;
                  break;
     case MATLAB_LANG: sprintf( buf, "%s.m", buf );
                  break;
-    default: printf("\n Language '%d' not implemented!\n",useLang); 
+    default: printf("\n Language '%d' not implemented!\n",useLang);
                  exit(1);
   }
   fp = fopen( buf, "r" );
   if ( fp == 0 )
     FatalError(3,"%s: Can't read file", buf );
   fclose(fp);
-    
+
   strcpy( cmd, "sed " );
-  
-  sprintf( cmd, "%s -e 's/KPP_ROOT/%s/g'", cmd, rootFileName );  
-  sprintf( cmd, "%s -e 's/KPP_NVAR/%d/g'", cmd, VarNr );  
-  sprintf( cmd, "%s -e 's/KPP_NFIX/%d/g'", cmd, FixNr );  
-  sprintf( cmd, "%s -e 's/KPP_NSPEC/%d/g'", cmd,SpcNr );  
-  sprintf( cmd, "%s -e 's/KPP_NREACT/%d/g'", cmd, EqnNr );  
-  sprintf( cmd, "%s -e 's/KPP_NONZERO/%d/g'", cmd, Jac_NZ );  
-  sprintf( cmd, "%s -e 's/KPP_LU_NONZERO/%d/g'", cmd, LU_Jac_NZ );  
-  sprintf( cmd, "%s -e 's/KPP_NHESS/%d/g'", cmd, Hess_NZ );  
-  
-  switch( useLang ) { 
-    case F77_LANG: 
-                 sprintf( cmd, "%s -e 's/KPP_REAL/%s/g'", cmd, F77_types[real] );  
+
+  sprintf( cmd, "%s -e 's/KPP_ROOT/%s/g'", cmd, rootFileName );
+  sprintf( cmd, "%s -e 's/KPP_NVAR/%d/g'", cmd, VarNr );
+  sprintf( cmd, "%s -e 's/KPP_NFIX/%d/g'", cmd, FixNr );
+  sprintf( cmd, "%s -e 's/KPP_NSPEC/%d/g'", cmd,SpcNr );
+  sprintf( cmd, "%s -e 's/KPP_NREACT/%d/g'", cmd, EqnNr );
+  sprintf( cmd, "%s -e 's/KPP_NONZERO/%d/g'", cmd, Jac_NZ );
+  sprintf( cmd, "%s -e 's/KPP_LU_NONZERO/%d/g'", cmd, LU_Jac_NZ );
+  sprintf( cmd, "%s -e 's/KPP_NHESS/%d/g'", cmd, Hess_NZ );
+
+  switch( useLang ) {
+    case F77_LANG:
+                 sprintf( cmd, "%s -e 's/KPP_REAL/%s/g'", cmd, F77_types[real] );
                  break;
-    case F90_LANG: 
-                 sprintf( cmd, "%s -e 's/KPP_REAL/%s/g'", cmd, F90_types[real] );  
+    case F90_LANG:
+                 sprintf( cmd, "%s -e 's/KPP_REAL/%s/g'", cmd, F90_types[real] );
                  break;
-    case C_LANG: 
-                 sprintf( cmd, "%s -e 's/KPP_REAL/%s/g'", cmd, C_types[real] );  
-                 break;  		             
-    case MATLAB_LANG: 
-                 break;  		             
-    default: printf("\n Language '%d' not implemented!\n",useLang); 
+    case C_LANG:
+                 sprintf( cmd, "%s -e 's/KPP_REAL/%s/g'", cmd, C_types[real] );
+                 break;
+    case MATLAB_LANG:
+                 break;
+    default: printf("\n Language '%d' not implemented!\n",useLang);
                  exit(1);
-  }           
-      
-  sprintf( cmd, "%s %s > %s", cmd, buf, tmpfile );  
+  }
+
+  sprintf( cmd, "%s %s > %s", cmd, buf, tmpfile );
 
   system( cmd );
   IncludeFile( tmpfile );
@@ -311,7 +312,7 @@ FILE * fp;
 void MapFunctionComment( int f, int *vars )
 {
 FILE *oldf;
- 
+
   oldf = UseFile( mapFile );
   FunctionStart( f, vars );
   /*NewLines(1);
@@ -325,10 +326,10 @@ int DefineVariable( char * name, int t, int bt, int maxi, int maxj, char * comme
 int i;
 VARIABLE * var;
 
-  for( i = 0; i < MAX_VAR; i++ ) 
+  for( i = 0; i < MAX_VAR; i++ )
     if( varTable[ i ] == 0 ) break;
-  
-  if( varTable[ i ] != 0 ) { 
+
+  if( varTable[ i ] != 0 ) {
     printf("\nVariable Table overflow");
     return -1;
   }
@@ -341,29 +342,26 @@ VARIABLE * var;
   var->maxj = maxj;
   var->value = -1;
   var->comment = comment;
-  
+
   varTable[ i ] = var;
-  return i;   
+  return i;
 }
 
 void FreeVariable( int n )
 {
-  if( varTable[ n ] ) { 
+  if( varTable[ n ] ) {
     free( varTable[ n ] );
     varTable[ n ] = 0;
-  } 
+  }
 }
- 
+
 NODE * Elm( int v, ... )
 {
 Va_list args;
 NODE *n;
 ELEMENT *elm;
 VARIABLE *var;
-int i, j;
-float val;
-char *expr;
-  
+
   var = varTable[ v ];
   n   = (NODE*)    malloc( sizeof(NODE) );
   elm = (ELEMENT*) malloc( sizeof(ELEMENT) );
@@ -373,20 +371,20 @@ char *expr;
   n->type = var->type;
   n->elm = elm;
   elm->var = v;
-    
+
   Va_start( args, v );
   switch( var->type ) {
     case CONST: switch( var->baseType ) {
                   case REAL: elm->val.cnst = (float)va_arg( args, double );
                              break;
-                  case INT:  elm->val.cnst = (float)va_arg( args, int );            
+                  case INT:  elm->val.cnst = (float)va_arg( args, int );
                 }
                 if( elm->val.cnst < 0 ) {
                   elm->val.cnst = -elm->val.cnst;
                   n->sign = -1;
                 }
                 break;
-    case ELM:   
+    case ELM:
                 break;
     case VELM:  elm->val.idx.i = va_arg( args, int );
                 break;
@@ -395,9 +393,9 @@ char *expr;
                 break;
     case EELM:  elm->val.expr = va_arg( args, char* );
                 break;
-  } 
+  }
   va_end( args );
-  
+
   return n;
 }
 
@@ -405,14 +403,14 @@ int IsConst( NODE *n, float val )
 {
   return ( ( n ) &&
            ( n->type == CONST ) &&
-           ( n->elm->val.cnst == val ) 
+           ( n->elm->val.cnst == val )
          );
 }
 
 NODE * BinaryOp( int op, NODE *n1, NODE *n2 )
 {
 NODE *n;
-  
+
   n   = (NODE*)    malloc( sizeof(NODE) );
   n->left = n1;
   n->right = n2;
@@ -426,59 +424,59 @@ NODE * Add( NODE *n1, NODE *n2 )
 {
   if( n1 == 0 ) return n2;
   if( n2 == 0 ) return n1;
-  
+
   if( IsConst( n1, 0 ) ) {
     FreeNode( n1 );
-    return n2;   
+    return n2;
   }
   if( IsConst( n2, 0 ) ) {
     FreeNode( n2 );
     return n1;
   }
-  return BinaryOp( ADD, n1, n2 );                                  
+  return BinaryOp( ADD, n1, n2 );
 }
 
 NODE * Sub( NODE *n1, NODE *n2 )
 {
   if( n1 == 0 ) return BinaryOp( SUB, 0, n2 );
   if( n2 == 0 ) return n1;
-  
+
   if( IsConst( n1, 0 ) ) {
     FreeNode( n1 );
-    return  BinaryOp( SUB, 0, n2 );   
+    return  BinaryOp( SUB, 0, n2 );
   }
   if( IsConst( n2, 0 ) ) {
     FreeNode( n2 );
     return n1;
   }
-  return BinaryOp( SUB, n1, n2 );                                  
+  return BinaryOp( SUB, n1, n2 );
 }
 
 NODE * Mul( NODE *n1, NODE *n2 )
 {
   if( n1 == 0 ) return n2;
   if( n2 == 0 ) return n1;
-  
-  if( IsConst( n1, 1 ) ) { 
+
+  if( IsConst( n1, 1 ) ) {
     n2->sign *= n1->sign;
     FreeNode( n1 );
-    return n2;   
+    return n2;
   }
   if( IsConst( n2, 1 ) ) {
     n2->sign *= n1->sign;
     FreeNode( n2 );
     return n1;
   }
-  if( IsConst( n1, 0 ) ) { 
+  if( IsConst( n1, 0 ) ) {
     FreeNode( n2 );
-    return n1;   
+    return n1;
   }
   if( IsConst( n2, 0 ) ) {
     FreeNode( n1 );
     return n2;
   }
-  
-  return BinaryOp( MUL, n1, n2 );                                  
+
+  return BinaryOp( MUL, n1, n2 );
 }
 
 NODE * Div( NODE *n1, NODE *n2 )
@@ -491,15 +489,15 @@ NODE * Div( NODE *n1, NODE *n2 )
     FreeNode( n2 );
     return n1;
   }
-                  
-  return BinaryOp( DIV, n1, n2 );    
+
+  return BinaryOp( DIV, n1, n2 );
 }
 
 NODE * Pow( NODE *n1, NODE *n2 )
 {
   if( n1 == 0 ) return n2;
   if( n2 == 0 ) return n1;
-  return BinaryOp( POW, n1, n2 );    
+  return BinaryOp( POW, n1, n2 );
 }
 
 void FreeNode( NODE * n )
@@ -550,7 +548,7 @@ NODE * NodeCopy( NODE *n1 )
 {
 NODE *n;
 ELEMENT *elm;
-  
+
   n   = (NODE*)    malloc( sizeof(NODE) );
   elm = (ELEMENT*) malloc( sizeof(ELEMENT) );
   *n = *n1;
@@ -578,7 +576,7 @@ NODE *cn;
   if( substENABLED == 0 ) {
     WriteElm( n );
     return;
-  }   
+  }
   cn = LookUpSubst( n );
   if( cn == 0 ) {
     WriteElm( n );
@@ -588,7 +586,7 @@ NODE *cn;
     } else {
       cn->type += SUBST;
       WriteSymbol( O_PAREN );
-      WriteNode( cn->right );       
+      WriteNode( cn->right );
       WriteSymbol( C_PAREN );
       cn->type -= SUBST;
     }
@@ -613,15 +611,15 @@ int needParen = 0;
   if( needParen ) WriteSymbol( C_PAREN );
 
   switch( n->type ) {
-    case ADD:  
-    case SUB:   
-    case MUL:   
-    case DIV:   
+    case ADD:
+    case SUB:
+    case MUL:
+    case DIV:
     case POW:   crtop = n->type;
                 break;
-    case NONE:  printf("ERROR - null element"); 
+    case NONE:  printf("ERROR - null element");
     		break;
-    case CONST: 
+    case CONST:
     case ELM:
     case VELM:
     case MELM:
@@ -658,12 +656,12 @@ int needParen = 0;
 
   if( ( n->right ) &&
       ( PRI[ n->right->type ] <= PRI[ n->type ] ) )
-      needParen = 1; 
+      needParen = 1;
 
   if( needParen ) {
     WriteOp( crtop );
     WriteSymbol( O_PAREN );
-  }  
+  }
   lastop = ExpandNode( n->right, n->type );
   if( needParen ) WriteSymbol( C_PAREN );
   return lastop;
@@ -687,8 +685,8 @@ char *olds;
 
   WriteAssign( ls, rs );
 
-  free( rs );  
-  free( ls );  
+  free( rs );
+  free( ls );
   FreeNode( lval );
   FreeNode( rval );
 }
@@ -696,12 +694,12 @@ char *olds;
 NODE * LookUpSubst( NODE *n )
 {
 NODE *cn;
- 
+
   cn = substList;
   while( cn != 0 ) {
-    if( NodeCmp( n, cn ) ) 
+    if( NodeCmp( n, cn ) )
       return cn;
-    cn = cn->left;  
+    cn = cn->left;
   }
   return 0;
 }
@@ -730,7 +728,7 @@ NODE *cn;
   pn = 0;
   cn = substList;
   while( cn != 0 ) {
-    if( NodeCmp( n, cn ) ) 
+    if( NodeCmp( n, cn ) )
       break;
     pn = cn;
     cn = cn->left;
@@ -740,9 +738,9 @@ NODE *cn;
   FreeNode( cn->right );
   if( pn )
     pn->left = cn->left;
-  else 
-    substList = cn->left;  
-  
+  else
+    substList = cn->left;
+
   cn->right = 0;
   cn->left = 0;
   FreeNode( cn );
@@ -757,7 +755,7 @@ NODE *n;
   while( n != 0 ) {
     printf("Subst: ");
     WriteElm( n );
-    printf( " --> " );    
+    printf( " --> " );
     WriteNode( n->right );
     printf("\n");
     n = n->left;
@@ -768,22 +766,20 @@ NODE *n;
 void CommentFncBegin( int f, int *vars )
 {
 VARIABLE *var;
-char * name;
 int narg;
 int i;
 
-  name = varTable[ f ]->name;
   narg = varTable[ f ]->maxi;
   var = varTable[ f ];
 
   WriteDelim();
   WriteComment("");
-  WriteComment("%s - %s", var->name, var->comment );   
+  WriteComment("%s - %s", var->name, var->comment );
   WriteComment("  Arguments :");
   for( i = 0; i < narg; i++ ) {
     var = varTable[vars[i]];
-    WriteComment("     %-10s- %s", var->name, var->comment ); 
-  }  
+    WriteComment("     %-10s- %s", var->name, var->comment );
+  }
   WriteComment("");
   WriteDelim();
   NewLines(1);
@@ -794,14 +790,12 @@ void CommentFunctionBegin( int f, ... )
 Va_list args;
 int i;
 int vars[20];
-char * name;
 int narg;
 
-  name = varTable[ f ]->name;
   narg = varTable[ f ]->maxi;
 
-  Va_start( args, f );  
-  for( i = 0; i < narg; i++ ) 
+  Va_start( args, f );
+  for( i = 0; i < narg; i++ )
     vars[i] = va_arg( args, int );
   va_end( args );
 
@@ -811,7 +805,7 @@ int narg;
 
 void CommentFunctionEnd( int f )
 {
-  WriteComment("End of %s function", varTable[ f ]->name );   
+  WriteComment("End of %s function", varTable[ f ]->name );
   WriteDelim();
   NewLines(2);
 }

commit 8e910d8f75a39f81adf413f2c29b8fdf509d730b

Recorrection of a mistake introduced in commit 32b4664: in gen.c, calling the function NonZero not only return a value, but also change the arguments! I commented by mistake an unused variable which was set equal to this function's returned value, but this obviously messed up things...
Plus very minor (cosmetic only) changes in Makefile and blas.f90
Sur la branche F77
	modifié :         kpp-2.2.3_01/src/gen.c
aucune modification n'a été ajoutée à la validation (utilisez "git add" ou "git co

kpp-2.2.3_01/src/Makefile
@@ -68,7 +68,7 @@ maintainer-clean: clean
 
 lex.yy.c: scan.l scan.h
 	@echo "  "$(FLEX) scan.l
-	@$(FLEX) -olex.yy.c scan.l 
+	@$(FLEX) -o lex.yy.c scan.l 
 
 y.tab.c: scan.y scan.h
 	@echo "  "$(BISON) scan.y

kpp-2.2.3_01/src/gen.c
@@ -1848,7 +1848,7 @@ int *irow;
 int *icol;
 int *crow;
 int *diag;
-/*int nElm;*/ /*jjb 12072017 defined but unused*/
+int nElm;
 int ibgn, iend;
 int useLangOld;
 int dim;
@@ -1864,7 +1864,9 @@ int dim;
 
   useLangOld = useLang;
   useLang = C_LANG;
-  /*nElm = NonZero( LU, 0, VarNr, irow, icol, crow, diag );*/ /*jjb 12072017 defined but unused*/
+  nElm = NonZero( LU, 0, VarNr, irow, icol, crow, diag );*/ /*jjb 01102017 nElm is not used but */
+                                    /*   icol, crow and diag are defined when calling NonZero */ 
+
   useLang = useLangOld;
 
   UseFile( linalgFile );
@@ -1917,7 +1919,7 @@ int *irow;
 int *icol;
 int *crow;
 int *diag;
-/*int nElm;*/ /*jjb 12072017 defined but unused*/
+int nElm;
 int ibgn, iend;
 int useLangOld;
 int **pos;
@@ -1935,7 +1937,8 @@ int dim;
 
   useLangOld = useLang;
   useLang = C_LANG;
-  /*nElm = NonZero( LU, 0, VarNr, irow, icol, crow, diag );*/ /*jjb 12072017 defined but unused*/
+  nElm = NonZero( LU, 0, VarNr, irow, icol, crow, diag ); /*jjb 01102017 nElm is not used but */
+                                    /*   icol, crow and diag are defined when calling NonZero */ 
   useLang = useLangOld;
 
   UseFile( linalgFile );

kpp-2.2.3_01/util/blas.f90
@@ -19,7 +19,7 @@
 !         CALL  SCOPY(N,X,1,Y,1)   or   CALL  DCOPY(N,X,1,Y,1)
 !--------------------------------------------------------------
 !     USE KPP_ROOT_Precision
-      
+
       INTEGER  :: i,incX,incY,M,MP1,N
       KPP_REAL :: X(N),Y(N)
 
@@ -31,7 +31,7 @@
           Y(i) = X(i)
         END DO
         IF( N .LT. 8 ) RETURN
-      END IF    
+      END IF
       MP1 = M+1
       DO i = MP1,N,8
         Y(i) = X(i)
@@ -78,7 +78,7 @@
         Y(i + 2) = Y(i + 2) + Alpha*X(i + 2)
         Y(i + 3) = Y(i + 3) + Alpha*X(i + 3)
       END DO
-      
+
       END SUBROUTINE WAXPY
 
 
@@ -86,7 +86,7 @@
 !--------------------------------------------------------------
       SUBROUTINE WSCAL(N,Alpha,X,incX)
 !--------------------------------------------------------------
-!     constant times a vector: x(1:N) <- Alpha*x(1:N) 
+!     constant times a vector: x(1:N) <- Alpha*x(1:N)
 !     only for incX=incY=1
 !     after BLAS
 !     replace this by the function from the optimized BLAS implementation:
@@ -163,7 +163,7 @@
       KPP_REAL, PARAMETER  ::  ONE=1.0_dp, HALF=0.5_dp
       LOGICAL, SAVE   ::  First=.TRUE.
 
-!$OMP THREADPRIVATE( Eps, First )      
+!$OMP THREADPRIVATE( Eps, First )
 
       IF (First) THEN
         First = .FALSE.
@@ -176,16 +176,16 @@
         PRINT*,'ERROR IN WLAMCH. EPS < ',Eps
         RETURN
 10      Eps = Eps*2
-        i = i-1      
+        i = i-1
       END IF
 
       WLAMCH = Eps
 
       END FUNCTION WLAMCH
-     
+
       SUBROUTINE WLAMCH_ADD( A, B, Suma )
 !      USE KPP_ROOT_Precision
-      
+
       KPP_REAL A, B, Suma
       Suma = A + B
 
@@ -199,7 +199,7 @@
 !     copies zeros into the vector y:  y <- 0
 !     after BLAS
 !--------------------------------------------------------------
-      
+
       INTEGER ::  i,M,MP1,N
       KPP_REAL ::  Y(N)
       KPP_REAL, PARAMETER :: ZERO = 0.0d0
@@ -212,7 +212,7 @@
           Y(i) = ZERO
         END DO
         IF( N .LT. 8 ) RETURN
-      END IF    
+      END IF
       MP1 = M+1
       DO i = MP1,N,8
         Y(i)     = ZERO
@@ -229,9 +229,9 @@
 
 
 !--------------------------------------------------------------
-      KPP_REAL FUNCTION WDOT (N, DX, incX, DY, incY) 
+      KPP_REAL FUNCTION WDOT (N, DX, incX, DY, incY)
 !--------------------------------------------------------------
-!     dot produce: wdot = x(1:N)*y(1:N) 
+!     dot produce: wdot = x(1:N)*y(1:N)
 !     only for incX=incY=1
 !     after BLAS
 !     replace this by the function from the optimized BLAS implementation:
@@ -241,52 +241,52 @@
 !--------------------------------------------------------------
       IMPLICIT NONE
       INTEGER :: N, incX, incY
-      KPP_REAL :: DX(N), DY(N) 
+      KPP_REAL :: DX(N), DY(N)
 
       INTEGER :: i, IX, IY, M, MP1, NS
-                                 
-      WDOT = 0.0D0 
-      IF (N .LE. 0) RETURN 
-      IF (incX .EQ. incY) IF (incX-1) 5,20,60 
-!                                                                       
-!     Code for unequal or nonpositive increments.                       
-!                                                                       
-    5 IX = 1 
-      IY = 1 
-      IF (incX .LT. 0) IX = (-N+1)*incX + 1 
-      IF (incY .LT. 0) IY = (-N+1)*incY + 1 
-      DO i = 1,N 
-        WDOT = WDOT + DX(IX)*DY(IY) 
-        IX = IX + incX 
-        IY = IY + incY 
-      END DO 
-      RETURN 
-!                                                                       
-!     Code for both increments equal to 1.                              
-!                                                                       
-!     Clean-up loop so remaining vector length is a multiple of 5.      
-!                                                                       
-   20 M = MOD(N,5) 
-      IF (M .EQ. 0) GO TO 40 
-      DO i = 1,M 
-         WDOT = WDOT + DX(i)*DY(i) 
-      END DO 
-      IF (N .LT. 5) RETURN 
-   40 MP1 = M + 1 
-      DO i = MP1,N,5 
-          WDOT = WDOT + DX(i)*DY(i) + DX(i+1)*DY(i+1) + DX(i+2)*DY(i+2) +  &
-                   DX(i+3)*DY(i+3) + DX(i+4)*DY(i+4)                   
-      END DO 
-      RETURN 
-!                                                                       
-!     Code for equal, positive, non-unit increments.                    
-!                                                                       
-   60 NS = N*incX 
-      DO i = 1,NS,incX 
-        WDOT = WDOT + DX(i)*DY(i) 
-      END DO 
 
-      END FUNCTION WDOT                                          
+      WDOT = 0.0D0
+      IF (N .LE. 0) RETURN
+      IF (incX .EQ. incY) IF (incX-1) 5,20,60
+!
+!     Code for unequal or nonpositive increments.
+!
+    5 IX = 1
+      IY = 1
+      IF (incX .LT. 0) IX = (-N+1)*incX + 1
+      IF (incY .LT. 0) IY = (-N+1)*incY + 1
+      DO i = 1,N
+        WDOT = WDOT + DX(IX)*DY(IY)
+        IX = IX + incX
+        IY = IY + incY
+      END DO
+      RETURN
+!
+!     Code for both increments equal to 1.
+!
+!     Clean-up loop so remaining vector length is a multiple of 5.
+!
+   20 M = MOD(N,5)
+      IF (M .EQ. 0) GO TO 40
+      DO i = 1,M
+         WDOT = WDOT + DX(i)*DY(i)
+      END DO
+      IF (N .LT. 5) RETURN
+   40 MP1 = M + 1
+      DO i = MP1,N,5
+          WDOT = WDOT + DX(i)*DY(i) + DX(i+1)*DY(i+1) + DX(i+2)*DY(i+2) +  &
+                   DX(i+3)*DY(i+3) + DX(i+4)*DY(i+4)
+      END DO
+      RETURN
+!
+!     Code for equal, positive, non-unit increments.
+!
+   60 NS = N*incX
+      DO i = 1,NS,incX
+        WDOT = WDOT + DX(i)*DY(i)
+      END DO
+
+      END FUNCTION WDOT
 
 
 !--------------------------------------------------------------
@@ -296,7 +296,7 @@
 !     BLAS - like
 !--------------------------------------------------------------
 !     USE KPP_ROOT_Precision
-      
+
       INTEGER :: i, M, MP1, N
       KPP_REAL :: X(N),Y(N),Z(N)
 
@@ -308,7 +308,7 @@
             Z(i) = X(i) + Y(i)
          END DO
          IF( N < 5 ) RETURN
-      END IF    
+      END IF
       MP1 = M+1
       DO i = MP1,N,5
          Z(i)     = X(i)     + Y(i)
@@ -319,15 +319,15 @@
       END DO
 
       END SUBROUTINE WADD
-      
-      
-      
+
+
+
 !--------------------------------------------------------------
       SUBROUTINE WGEFA(N,A,Ipvt,info)
 !--------------------------------------------------------------
 !     WGEFA FACTORS THE MATRIX A (N,N) BY
 !           GAUSS ELIMINATION WITH PARTIAL PIVOTING
-!     LINPACK - LIKE 
+!     LINPACK - LIKE
 !--------------------------------------------------------------
 !
       INTEGER       :: N,Ipvt(N),info
@@ -339,7 +339,7 @@
       info = 0
 
 size: IF (n > 1) THEN
-      
+
 col:  DO k = 1, n-1
 
 !        find l = pivot index
@@ -357,7 +357,7 @@ col:  DO k = 1, n-1
          IF (ABS(A(l,k)) < TINY(ZERO)) THEN
             info = k
             return
-         ELSE   
+         ELSE
             IF (l /= k) THEN
                t = A(l,k); A(l,k) = A(k,k); A(k,k) = t
             END IF
@@ -369,16 +369,16 @@ col:  DO k = 1, n-1
                   A(l,j) = A(k,j); A(k,j) = t
                END IF
                CALL WAXPY(n-k,t,A(k+1,k),1,A(k+1,j),1)
-            END DO         
+            END DO
          END IF
-         
+
        END DO col
-       
+
       END IF size
-      
+
       Ipvt(N) = N
       IF (ABS(A(N,N)) == ZERO) info = N
-      
+
       END SUBROUTINE WGEFA
 
 
@@ -391,7 +391,7 @@ col:  DO k = 1, n-1
 !
 !     Trans      = 'N'   to solve  A*x = b ,
 !                = 'T'   to solve  transpose(A)*x = b
-!     LINPACK - LIKE 
+!     LINPACK - LIKE
 !--------------------------------------------------------------
 
       INTEGER       :: N,Ipvt(N)
@@ -400,7 +400,7 @@ col:  DO k = 1, n-1
       KPP_REAL :: t
       INTEGER       :: k,kb,l
 
-      
+
       SELECT CASE (Trans)
 
       CASE ('n','N')  !  Solve  A * x = b
@@ -424,7 +424,7 @@ col:  DO k = 1, n-1
             t = -b(k)
             CALL WAXPY(k-1,t,a(1,k),1,b(1),1)
          END DO
-      
+
       CASE ('t','T')  !  Solve transpose(A) * x = b
 
 !        first solve  trans(U)*y = b
@@ -443,7 +443,7 @@ col:  DO k = 1, n-1
             END IF
          END DO
          END IF
-   
+
       END SELECT
 
       END SUBROUTINE WGESL