5e8c5ae76dfc3be33330edf3b463ee4fd1bdc1f7
Bob Yantosca <yantosca@seas.harvard.edu>
2021-02-11 12:40:14 -0500

    Update version number to 2.3.0_gc
    In src/gdata.h, we have updated the KPP version number to 2.3.0_gc,
    to denote the following updates:
    
    1) Do not add calls to UPDATE_RCONST in int/rosenbrock*.F90
    2) Write Fortran files with the .F90 extension instead of .f90
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- kpp-2.2.3_01/src/gdata.h
+++ kpp-2.2.3_01/src/gdata.h
@@ -29,24 +29,22 @@
 
 ******************************************************************************/
 
-#define KPP_VERSION "2.2.5_gc"  /* bmy, 04 Jan 2021 */
+#define KPP_VERSION "2.3.0_gc" /* KPP 2.3.0_gc, Bob Yantosca (11 Feb 2020)  */
 
 #ifndef _GDATA_H_
 #define _GDATA_H_
 
 #include <stdio.h>
 
-#define MAX_EQN         1000
-#define MAX_SPECIES     800
+#define MAX_EQN        1500    /* KPP 2.3.0_gc, Bob Yantosca (11 Feb 2020)  */
+#define MAX_SPECIES    1000    /* KPP 2.3.0_gc, Bob Yantosca (11 Feb 2020)  */
 #define MAX_SPNAME       30
 #define MAX_IVAL         40
-/* MAX_EQNTAG = max length of equation ID in eqn file */
-#define MAX_EQNTAG       12
-/* MAX_K = max length of rate expression in eqn file */
-#define MAX_K           150
+#define MAX_EQNTAG       12    /* Max length of equation ID in eqn file     */
+#define MAX_K           150    /* Max length of rate expression in eqn file */
 #define MAX_ATOMS	 10
 #define MAX_ATNAME	 10
-#define MAX_ATNR	250 
+#define MAX_ATNR	250
 #define MAX_PATH        120
 #define MAX_FILES	 20
 #define MAX_FAMILIES	300
@@ -62,7 +60,7 @@
 #define IntegName(x) FileName((x),"INTEG","int",".def")
 
 enum krtypes { NUMBER, EXPRESION, PHOTO };
-enum table_modes { F_TEXT, FC_TEXT, C_TEXT, S_TEXT }; 
+enum table_modes { F_TEXT, FC_TEXT, C_TEXT, S_TEXT };
 enum lang { NO_LANG, C_LANG, F77_LANG, F90_LANG, MATLAB_LANG };
 enum inl_code { F77_GLOBAL,    F77_INIT,    F77_DATA,    F77_UTIL,    F77_RATES,    F77_RCONST,
 	      F90_GLOBAL,    F90_INIT,    F90_DATA,    F90_UTIL,    F90_RATES,    F90_RCONST,
@@ -71,9 +69,9 @@ enum inl_code { F77_GLOBAL,    F77_INIT,    F77_DATA,    F77_UTIL,    F77_RATES,
 	      INLINE_OPT
 	      };
 
-enum jacobian_format { JAC_OFF, JAC_FULL, JAC_LU_ROW, JAC_ROW };	      
+enum jacobian_format { JAC_OFF, JAC_FULL, JAC_LU_ROW, JAC_ROW };
 
-               	      
+
 typedef short int CODE;
 typedef float EQ_VECT[ MAX_EQN ];
 
@@ -85,7 +83,7 @@ typedef struct {
 
 typedef struct {
                  unsigned char code;
-                 unsigned char nr; 
+                 unsigned char nr;
                } ATOM;
 
 typedef struct {
@@ -96,7 +94,7 @@ typedef struct {
                  short int nratoms;
 		 char name[ MAX_SPNAME ];
                  char ival[ MAX_IVAL ];
-                 ATOM atoms[ MAX_ATOMS ]; 
+                 ATOM atoms[ MAX_ATOMS ];
                  int flux; /* msl_290416 */
 	       } SPECIES_DEF;
 
@@ -114,7 +112,7 @@ typedef struct {
                  short int nrmembers;
 		 char name[ MAX_SPNAME ];
                  char ival[ MAX_IVAL ];
-                 MEMBER members[ MAX_MEMBERS ]; 
+                 MEMBER members[ MAX_MEMBERS ];
 	       } FAMILY_DEF;
 
 typedef struct {
@@ -140,7 +138,7 @@ extern int AtomNr;
 extern int VarNr;
 extern int VarActiveNr;
 extern int FixNr;
-extern int plNr; 
+extern int plNr;
 extern int VarStartNr;
 extern int FixStartNr;
 extern int Hess_NZ;
@@ -170,9 +168,9 @@ extern int useLang;
 extern int useStochastic;
 extern int doFlux;
 
-/* if useValues=1 KPP replaces parameters like NVAR etc. 
+/* if useValues=1 KPP replaces parameters like NVAR etc.
        by their values in vector/matrix declarations */
-extern int useDeclareValues; 
+extern int useDeclareValues;
 
 extern char Home[ MAX_PATH ];
 extern char integrator[ MAX_PATH ];
@@ -239,4 +237,3 @@ void FreeIntegerMatrix ( int** mat, int m, int n );
 int Index( int i );
 
 #endif
-
