6cde3d7d5d95b2a90e324a5cc6d0f55b18835a18
Bob Yantosca <yantosca@seas.harvard.edu>
2021-06-11 15:04:55 -0400

    Change version number to 2.3.3_gc in gdata.h
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- kpp-code/src/gdata.h
+++ kpp-code/src/gdata.h
@@ -29,7 +29,7 @@
 
 ******************************************************************************/
 
-#define KPP_VERSION "2.3.2_gc"  // Version number
+#define KPP_VERSION "2.3.3_gc"  // Version number
 
 #ifndef _GDATA_H_
 #define _GDATA_H_
