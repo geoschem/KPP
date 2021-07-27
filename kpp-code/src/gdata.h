5399e7f237516648eae9229e031b06b2dad484e9
Bob Yantosca <yantosca@seas.harvard.edu>
2021-07-27 16:19:14 -0400

    Change version number to 3.0.0_gc in kpp-code/src/gdata.h
    In anticipation of KPP becoming a submodule of GEOS-Chem (and the
    build system being converted from GNU Make to CMake), we have
    changed the version number from 2.3.3_gc to 3.0.0_gc.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- kpp-code/src/gdata.h
+++ kpp-code/src/gdata.h
@@ -29,7 +29,7 @@
 
 ******************************************************************************/
 
-#define KPP_VERSION "2.3.3_gc"  // Version number
+#define KPP_VERSION "3.0.0_gc"  // Version number
 
 #ifndef _GDATA_H_
 #define _GDATA_H_
